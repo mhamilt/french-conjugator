1aea0dd7cdde646ab76dc0803d3261d31bf63990
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-11-11 13:27:07 +0000

    std conjugation tags

--- make_verbs.py
+++ make_verbs.py
@@ -1,53 +1,43 @@
+# Make Verbs
+#
+#   Take the verbs.csv file and transform into a JSON file
+# -----------------------------------------------------------------------------
 import json
 import csv
 from pprint import pprint
 import unicodedata as ud
-
+# -----------------------------------------------------------------------------
 def strip_accents(s):
    return ''.join(c for c in ud.normalize('NFD', s)
                   if ud.category(c) != 'Mn')
+# -----------------------------------------------------------------------------
+if __name__ == "__main__":
+    verbs = {}
 
-verbs = {}
+    conjugations = ["INF","GER","PRS.PTCP","PST.PTCP","AUX","PRS.IND.1SG","PRS.IND.2SG","PRS.IND.3SG","PRS.IND.1PL","PRS.IND.2PL","PRS.IND.3PL","IPRF.IND.1SG","IPRF.IND.2SG","IPRF.IND.3SG","IPRF.IND.1PL","IPRF.IND.2PL","IPRF.IND.3PL","PRET.IND.1SG","PRET.IND.2SG","PRET.IND.3SG","PRET.IND.1PL","PRET.IND.2PL","PRET.IND.1PL","FUT.IND.1SG","FUT.IND.2SG","FUT.IND.3SG","FUT.IND.1PL","FUT.IND.2PL","FUT.IND.3PL","COND.1SG","COND.2SG","COND.3SG","COND.1PL","COND.2PL","COND.3PL","PRS.SBJV.1SG","PRS.SBJV.2SG","PRS.SBJV.3SG","PRS.SBJV.1PL","PRS.SBJV.2PL","PRS.SBJV.3PL","IPRF.IND.1SG","IPRF.IND.2SG","IPRF.IND.3SG","IPRF.IND.1PL","IPRF.IND.2PL","IPRF.IND.3PL","IMP.1SG","IMP.2SG","IMP.3SG","IMP.1PL","IMP.2PL","IMP.3PL"]
 
-with open('verbs.csv') as csvfile:
-    data = csv.reader(csvfile, delimiter=',')
+    with open('verbs.csv') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        next(data) # burn header
 
-    for row in data:
-        if row[0] == "pouvoir":
-            print(row)
+        for row in data:
+            if row[0] == "pouvoir":
+                print(row)
 
-        verbs[row[0]] = {
-            "INF": row[0], "GER": row[1], "PRS.PTCP": row[2], "PST.PTCP": row[3],
-            "IMP.1SG": row[47], "IMP.2SG": row[48], "IMP.3SG": row[49],
-            "IMP.1PL": row[50], "IMP.2PL": row[51], "IMP.3PL": row[52],
-            "PRS.IND.1SG": row[5], "PRS.IND.2SG": row[6], "PRS.IND.3SG": row[7],
-            "PRS.IND.1PL": row[8], "PRS.IND.2PL": row[9], "PRS.IND.3PL": row[10],
-            "PRS.SBJV.1SG": row[35], "PRS.SBJV.2SG": row[36], "PRS.SBJV.3SG": row[37],
-            "PRS.SBJV.1PL": row[38], "PRS.SBJV.2PL": row[39], "PRS.SBJV.3PL": row[40],
-            "IPRF.IND.1SG": row[11], "IPRF.IND.2SG": row[12], "IPRF.IND.3SG": row[13],
-            "IPRF.IND.1PL": row[14], "IPRF.IND.2PL": row[15], "IPRF.IND.3PL": row[16],
-            "IPRF.SBJV.1SG": row[41], "IPRF.SBJV.2SG": row[42], "IPRF.SBJV.3SG": row[43],
-            "IPRF.SBJV.1PL": row[44], "IPRF.SBJV.2PL": row[45], "IPRF.SBJV.3PL": row[46],
-            "PRET.IND.1SG": row[17], "PRET.IND.2SG": row[18], "PRET.IND.3SG": row[19],
-            "PRET.IND.1PL": row[20], "PRET.IND.2PL": row[21], "PRET.IND.3PL": row[22],
-            "FUT.IND.1SG": row[23], "FUT.IND.2SG": row[24], "FUT.IND.3SG": row[25],
-            "FUT.IND.1PL": row[26], "FUT.IND.2PL": row[27], "FUT.IND.3PL": row[28],
-            "COND.1SG": row[29], "COND.2SG": row[30], "COND.3SG": row[31],
-            "COND.1PL": row[32], "COND.2PL": row[33], "COND.3PL": row[34]
-        }
+            verbs[row[0]] = {key: row[conjugations.index(key)] for key in conjugations}        
 
-print(len(verbs.keys()))
+    print(len(verbs.keys()))
 
-with open('data.json', 'w') as f:
-    json.dump(verbs, f, ensure_ascii=False)
+    with open('verbs.json', 'w') as f:
+        json.dump(verbs, f, ensure_ascii=False)
 
-# Read in the file
-with open('data.json', 'r') as file:
-    filedata = file.read()
+    # Read in the file
+    with open('verbs.json', 'r') as file:
+        filedata = file.read()
 
-# Replace the target string
-filedata = filedata.replace('},', '},\n')
+    # Replace the target string
+    filedata = filedata.replace('},', '},\n')
 
-# Write the file out again
-with open('data.json', 'w') as file:
-    file.write(filedata)
+    # Write the file out again
+    with open('verbs.json', 'w') as file:
+        file.write(filedata)
